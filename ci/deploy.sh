3fa41064fda951c8e3c66c581c87ab5f69d3e8fe
esteban <esteban.baron@etu.umontpellier.fr>
2022-05-16 21:57:36 +0200

    (feat): Add deploy script for ci
    Signed-off-by: esteban <esteban.baron@etu.umontpellier.fr>

---
+++ ci/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# init ssh connection
+ssh esteban@162.38.112.131
+
+# update polycode-frontend repo
+cd ~esteban/polycode/polycode-frontend
+git pull --rebase
+
+# deploy with docker compose file
+sudo su -
+cd ~esteban/polycode
+docker compose --env-file ./config/.env.prod up -d --build
+
+# back to esteban user
+exit
+
+# close ssh connection
+exit 
