7e7737c61a81dc6699ed5cdf724f56ff9132e0e9
esteban <esteban.baron@etu.umontpellier.fr>
2022-05-17 23:09:42 +0200

    (fix): update deploy.sh
    Signed-off-by: esteban <esteban.baron@etu.umontpellier.fr>

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 
 # init ssh connection
-ssh -tt -o StrictHostKeyChecking=no esteban@162.38.112.131 << EOF
+
+ssh -tt -o StrictHostKeyChecking=no esteban@162.38.112.131 "
 
 # update polycode-frontend repo
-cd ~esteban/polycode/polycode-frontend
-git pull --rebase
+cd ~esteban/polycode/polycode-frontend && 
+git pull --rebase && 
 
 # deploy with docker compose file
-sudo su -
-cd ~esteban/polycode
-docker compose --env-file ./config/.env.prod up -d --build
-
-# back to esteban user
-exit
+cd ~esteban/polycode &&
+docker compose --env-file ./config/.env.prod up -d --build && 
 
 # close ssh connection
 exit 
+"
 
-EOF
+
+# ssh -tt -o StrictHostKeyChecking=no esteban@162.38.112.131 << EOF
+
+# # update polycode-frontend repo
+# cd ~esteban/polycode/polycode-frontend
+# git pull --rebase
+
+# # deploy with docker compose file
+# sudo su -
+# cd ~esteban/polycode
+# docker compose --env-file ./config/.env.prod up -d --build
+
+# # back to esteban user
+# exit
+
+# # close ssh connection
+# exit 
+
+# EOF
