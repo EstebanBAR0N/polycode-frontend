9465fa940a3d14a4d76792fa0c7d1c7ae1e137f1
esteban <esteban.baron@etu.umontpellier.fr>
2022-05-17 20:36:21 +0200

    (fix): remove useless imports
    Signed-off-by: esteban <esteban.baron@etu.umontpellier.fr>

--- src/components/other/exercisePageComponents/OutputChoice.tsx
+++ src/components/other/exercisePageComponents/OutputChoice.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import * as React from 'react';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
